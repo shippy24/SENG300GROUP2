bf6eeabed9e57424b85d48c6927d2928ce4f710c
Preston <preston.haff@gmail.com>
2018-03-22 22:59:43 -0600

    put comments in typeObjQueue.java to explain building linkedlist

--- src/assignment2/typeObjQueue.java
+++ src/assignment2/typeObjQueue.java
@@ -8,34 +8,34 @@ public class typeObjQueue {
 	public LinkedList<typeobj> types = new LinkedList<typeobj>();
 	
 	public void upDateListDec (String qns) {
-		boolean flag = true;
-		for(typeobj X: types) {
-			if(X.name.contentEquals(qns)) {
-				X.updateDeclarationCount();
-				flag = false;
+		boolean flag = true;				//flag set
+		for(typeobj X: types) {				//iterate through linkedlist
+			if(X.name.equals(qns)) {		//check if any element's name equals qualifiedName
+				X.updateDeclarationCount();	//if yes up date that element's referenceCount
+				flag = false;				//set flag to false
 			}
 			break;
 		}
-		if(flag) {
-			typeobj Y = new typeobj(qns);
-			Y.updateDeclarationCount();
-			types.add(Y);
+		if(flag) {							//if element not present
+			typeobj Y = new typeobj(qns);	//create element
+			Y.updateDeclarationCount();		//increment referenceCount
+			types.add(Y);					//add element to linkedlist
 		}
 	}
 	
 	public void upDateListRef (String qns) {
-		boolean flag = true;
-		for(typeobj X: types) {
-			if(X.name.contentEquals(qns)) {
-				X.updateReferenceCount();
-				flag = false;
+		boolean flag = true;				//flag set
+		for(typeobj X: types) {				//iterate through linkedlist
+			if(X.name.equals(qns)) {		//check if any element's name equals qualifiedName
+				X.updateReferenceCount();	//if yes up date that element's referenceCount
+				flag = false;				//set flag to false
 			}
 			break;
 		}
-		if(flag) {
-			typeobj Y = new typeobj(qns);
-			Y.updateReferenceCount();
-			types.add(Y);
+		if(flag) {							//if element not present
+			typeobj Y = new typeobj(qns);	//create element
+			Y.updateReferenceCount();		//increment referenceCount
+			types.add(Y);					//add element to linkedlist
 		}
 	}
 }
